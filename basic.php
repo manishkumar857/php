86725b112d8399a962c4e8a5a3c7b69b202b0626
Manish <jangramanish2602@gmail.com>
2025-01-14 09:41:57 +0530

    basic part 2

--- basic.php
+++ basic.php
@@ -16,11 +16,14 @@
     echo $y;
     echo "<br>";
 
+    echo var_dump($x); #to get variable type
+
     echo "the value of x is $x";
     echo "<br>";
     echo "the value of y is ".$y."<br>";
 
     echo "the sum of x and y is ".$x+$y;
     ?>
+
     </body>
     </html>
