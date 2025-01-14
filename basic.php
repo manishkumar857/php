f56857177167f079841c1b8b89951fe5d1aad3bb
Manish <jangramanish2602@gmail.com>
2025-01-14 09:32:36 +0530

    php basics

---
+++ basic.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <?php
+    $txt = "Hello World";
+    $x=5;
+    $y=10.5;
+
+    #comment
+    //comment
+
+    echo $txt;
+    echo "<br>";
+    echo $x;
+    echo "<br>";
+    echo $y;
+    echo "<br>";
+
+    echo "the value of x is $x";
+    echo "<br>";
+    echo "the value of y is ".$y."<br>";
+
+    echo "the sum of x and y is ".$x+$y;
+    ?>
+    </body>
+    </html>
---
+++ file.php
@@ -0,0 +1,13 @@
+<doctype html5>
+<html>
+<head>
+<title>welcome to php</title>
+</head>
+<body bgcolor="green">
+<h1>php</h1>
+<?php
+   echo "welcome" ."<br>"."to php"."<br>";
+   print "welcome "."<br>"."to php";
+?>
+</body>
+</html>
