4c7bc3fa113bfbcdf14bd27919220623f6d54d98
Manish <jangramanish2602@gmail.com>
2025-01-17 09:55:56 +0530

    conditional

---
+++ conditional.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html >
+<head>
+</head>
+<body>
+
+<?php
+// $marks=85;
+// if($marks>=90){
+//     echo "Grade:A";
+// }elseif($marks>=75){
+//     echo "Grade B";
+// }else{
+//     echo "Grade C";}
+
+
+
+
+//short hand if statement 
+// $a=89;
+// if($a>70)  $b="hello" ; echo"a is greater"
+
+//ternary operator
+
+// $y=6;
+// $x= $y<5? "hello":"World";
+//  echo$x;
+
+
+//switch case 
+$day=3;
+switch ($day) {
+    case 1:
+        echo"Monday";
+        break;
+        case 2:
+            echo "Tuesday";
+            break;
+            case 3:
+                echo "Wednesday";
+                break;
+                default:
+                echo "Invalid day";
+}
+
+    ?>
+ 
+</body>
+</html>
+
+
+
+
