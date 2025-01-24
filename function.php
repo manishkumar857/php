d8ae745335d3f6cc011768652ee56a722608104f
Manish <jangramanish2602@gmail.com>
2025-01-24 09:27:35 +0530

    function

---
+++ function.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $sum=0;
+    function printodd(){
+        for($i=0;$i<31;$i++)
+        {
+            if($i%2!=0){
+                echo $i." ";
+                global $sum;
+                $sum=$sum+$i;
+            }
+        }
+        echo $sum;
+    }
+    printodd();
+    ?>
+</body>
+</html>
