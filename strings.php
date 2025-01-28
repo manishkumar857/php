18aa6d5c6ece23ed40392ed3f8d51d3f882742e0
Manish <jangramanish2602@gmail.com>
2025-01-28 10:16:56 +0530

    string

---
+++ strings.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <?php
+    $a="Hello World";
+    echo strlen($a)."<br>";
+    echo str_word_count($a)."<br>";
+
+    echo strpos($a,"World"); //find len of word in string
+
+
+    ?>
+</body>
+</html>
