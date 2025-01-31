696c24abd6bc89a3b4ecdef128fb42220da9d7fc
Manish <jangramanish2602@gmail.com>
2025-01-31 09:17:19 +0530

    string function

--- strings.php
+++ strings.php
@@ -2,12 +2,18 @@
 <html>
 <body>
     <?php
-    $a="Hello World";
+    $a="  Hello World";
     echo strlen($a)."<br>";
     echo str_word_count($a)."<br>";
 
     echo strpos($a,"World"); //find len of word in string
 
+    echo strtoupper($a)."<br>";
+    echo strtolower($a)."<br>";
+
+    echo str_replace("World","Earth",$a)."<br>";
+    echo strrev($a)."<br>";
+    echo trim($a);//remove whitespaces from begining or end
 
     ?>
 </body>
