3d1e729ac44da03c9758e6c7641442812a5b7c39
Manish <jangramanish2602@gmail.com>
2025-01-31 09:37:24 +0530

    Anonmymous Function

--- function.php
+++ function.php
@@ -1,65 +1,72 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
     <title>Document</title>
 </head>
 <body>
     <?php
-    $sum=0;
-    function printodd(){
-        for($i=0;$i<31;$i++)
-        {
-            if($i%2!=0){
-                echo $i." ";
-                global $sum;
-                $sum=$sum+$i;
-            }
-        }
-        echo $sum;
-    }
-    printodd();
-    ?>
+    // $sum=0;
+    // function printodd(){
+    //     for($i=0;$i<31;$i++)
+    //     {
+    //         if($i%2!=0){
+    //             echo $i." ";
+    //             global $sum;
+    //             $sum=$sum+$i;
+    //         }
+    //     }
+    //     echo $sum;
+    // }
+    // printodd();
+    // ?>
     <?php
     
-    function area($radius){
-        echo 3.14*$radius**2;
-    }
-    area(27);
+    // function area($radius){
+    //     echo 3.14*$radius**2;
+    // }
+    // area(27);
     ?> 
 
     <?php
-   function strconvart($string,$isupper){
-    if($isupper){
-        return strtoupper($string);
-    }else{
-        return strtolower($string);
-    }}
-    $string = "Hello World! ";
-    $isupper = true;
-    $result = strconvart($string,$isupper);
-    echo $result;
-    $isupper = false;
-    $result = strconvart($string,$isupper);
-    echo $result;
+//    function strconvart($string,$isupper){
+//     if($isupper){
+//         return strtoupper($string);
+//     }else{
+//         return strtolower($string);
+//     }}
+//     $string = "Hello World! ";
+//     $isupper = true;
+//     $result = strconvart($string,$isupper);
+//     echo $result;
+//     $isupper = false;
+//     $result = strconvart($string,$isupper);
+//     echo $result;
     ?>
 
 
     <?php
     #variable function
     
-    function hello(){
-        echo"Hello Variable Function !!!\n";
-    }
-    function a()
-    {
-        echo "Hello normal function \n";
-    }
+    // function hello($a,$b){
+    //     echo"Hello Variable Function !!!\n";
+    //     echo $a+$b;
+    // }
+    // function a()
+    // {
+    //     echo "Hello normal function \n";
+    // }
 
-    $func="hello";
-    $func();
-    a();
+    // $func="hello";
+    // $func(4,5);
+    // a();
+
+    #Anonymys function
+    $noname = function()
+    {
+        return "Hello, Anonmymous Function!";
+    };
+    echo $noname();
     ?>
 </body>
 </html>
