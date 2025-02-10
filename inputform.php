daac26da1072270ab11185c910ceaaae90625268
Manish <jangramanish2602@gmail.com>
2025-02-10 09:29:03 +0530

    input form

---
+++ inputform.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Input form</Input></title>
+</head>
+<body>
+
+    <form method="post" action="process.php">
+        <label for ="name">Enter your name:</label>
+        <input type = "text" name="name" id="name" required>
+        <button type = "sumbit">Submit</button>
+    </form>
+</body>
+</html>
+    
+</body>
+</html>
